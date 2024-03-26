0c9b8c3fa29299c8ebaf199d45c647d35258a0be
ihsankv <ihsankv263@gmail.com>
2024-03-27 00:12:27 +0530

    follow unfollow options

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -20,110 +20,139 @@ import { useCallback, useMemo } from "react";
 // } from "@/graphql/mutation/user";
 import { useQueryClient } from "@tanstack/react-query";
 import { getUserByIdQuery } from "@/graphql/query/user";
+import { followUserMutation, unfollowUserMutation } from "@/graphql/mutation/user";
 
 interface ServerProps {
-    userInfo?: User;
+  userInfo?: User;
 }
 
 const UserProfilePage: NextPage<ServerProps> = (props) => {
-    console.log("props",props)
-    // const UserProfilePage: NextPage = () => {
+  console.log("props", props)
+  // const UserProfilePage: NextPage = () => {
 
-    const { user } = useCurrentUser()
-    console.log(user)
-    const router = useRouter()
-    console.log(router)
+  const { user } = useCurrentUser()
+  console.log(user)
+  const router = useRouter()
+  const { user: currentUser } = useCurrentUser()
+  const queryClient = useQueryClient();
+
+  // console.log(router)
+  const amIFollowing = useMemo(() => {
+    if (!props.userInfo) return false;
     return (
+      (currentUser?.following?.findIndex(
+        (el:any) => el?.id === props.userInfo?.id
+      ) ?? -1) >= 0
+    );
+  }, [currentUser?.following, props.userInfo]);
+
+  const handleFollowUser = useCallback(async () => {
+    if (!props.userInfo?.id) return;
+
+    await graphqlClient.request(followUserMutation, { to: props.userInfo?.id });
+    await queryClient.invalidateQueries({ queryKey: ['curent-user'] });
+  }, [props.userInfo?.id, queryClient]);
+
+  const handleUnfollowUser = useCallback(async () => {
+    if (!props.userInfo?.id) return;
+
+    await graphqlClient.request(unfollowUserMutation, {
+      to: props.userInfo?.id,
+    });
+    await queryClient.invalidateQueries({ queryKey: ['curent-user'] });;
+  }, [props.userInfo?.id, queryClient]);
+
+  return (
+    <div>
+      <Twitterlayout>
         <div>
-            <Twitterlayout>
-                <div>
-                    <nav className="flex items-center gap-3 py-3 px-3">
-                        <BsArrowLeftShort className="text-4xl" />
-                        <div>
-                            <h1 className="text-2xl font-bold">
-                                {/* {user?.firstName} */}
-                                {props.userInfo?.firstName} {props.userInfo?.lastName}
-                            </h1>
-                            <h1 className="text-md font-bold text-slate-500">
-                                {props.userInfo?.tweets?.length}
-                                Tweets
-                            </h1>
-                        </div>
-                    </nav>
-                    <div className="p-4 border-b border-slate-800">
-                        {props.userInfo?.profileImageURL && (
-                <Image
-                  src={props.userInfo?.profileImageURL}
-                  alt="user-image"
-                  className="rounded-full"
-                  width={100}
-                  height={100}
-                />
+          <nav className="flex items-center gap-3 py-3 px-3">
+            <BsArrowLeftShort className="text-4xl" />
+            <div>
+              <h1 className="text-2xl font-bold">
+                {/* {user?.firstName} */}
+                {props.userInfo?.firstName} {props.userInfo?.lastName}
+              </h1>
+              <h1 className="text-md font-bold text-slate-500">
+                {props.userInfo?.tweets?.length}
+                Tweets
+              </h1>
+            </div>
+          </nav>
+          <div className="p-4 border-b border-slate-800">
+            {props.userInfo?.profileImageURL && (
+              <Image
+                src={props.userInfo?.profileImageURL}
+                alt="user-image"
+                className="rounded-full"
+                width={100}
+                height={100}
+              />
+            )}
+            <h1 className="text-2xl font-bold mt-5">
+              {props.userInfo?.firstName} {props.userInfo?.lastName}
+            </h1>
+            <div className="flex justify-between items-center">
+              <div className="flex gap-4 mt-2 text-sm text-gray-400">
+                <span>{props.userInfo?.followers?.length} followers</span>
+                <span>{props.userInfo?.following?.length} following</span>
+              </div>
+              {currentUser?.id !== props.userInfo?.id && (
+                <>
+                  {amIFollowing ? (
+                    <button
+                      onClick={handleUnfollowUser}
+                      className="bg-white text-black px-3 py-1 rounded-full text-sm"
+                    >
+                      Unfollow
+                    </button>
+                  ) : (
+                    <button
+                      onClick={handleFollowUser}
+                      className="bg-white text-black px-3 py-1 rounded-full text-sm"
+                    >
+                      Follow
+                    </button>
+                  )}
+                </>
               )}
-                        <h1 className="text-2xl font-bold mt-5">
-                            {props.userInfo?.firstName} {props.userInfo?.lastName}
-                        </h1>
-                        <div className="flex justify-between items-center">
-                            <div className="flex gap-4 mt-2 text-sm text-gray-400">
-                                {/* <span>{props.userInfo?.followers?.length} followers</span> */}
-                                {/* <span>{props.userInfo?.following?.length} following</span> */}
-                            </div>
-                            {/* {currentUser?.id !== props.userInfo?.id && (
-                  <>
-                    {amIFollowing ? (
-                      <button
-                        onClick={handleUnfollowUser}
-                        className="bg-white text-black px-3 py-1 rounded-full text-sm"
-                      >
-                        Unfollow
-                      </button>
-                    ) : (
-                      <button
-                        onClick={handleFollowUser}
-                        className="bg-white text-black px-3 py-1 rounded-full text-sm"
-                      >
-                        Follow
-                      </button>
-                    )}
-                  </>
-                )} */}
-                        </div>
-                    </div>
-                    <div>
-                        {user?.tweets?.map((tweet) => (
-                            <FeedCard data={tweet as Tweet} key={tweet?.id} />
-                        ))}
-                    </div>
-                    {/* <div>
+            </div>
+          </div>
+          <div>
+            {user?.tweets?.map((tweet) => (
+              <FeedCard data={tweet as Tweet} key={tweet?.id} />
+            ))}
+          </div>
+          {/* <div>
               {props.userInfo?.tweets?.map((tweet) => (
                 <FeedCard data={tweet as Tweet} key={tweet?.id} />
               ))}
             </div> */}
-                </div>
-            </Twitterlayout>
         </div>
-    )
+      </Twitterlayout>
+    </div>
+  )
 }
 
 
 export const getServerSideProps: GetServerSideProps<ServerProps> = async (
-    // export const getServerSideProps: GetServerSideProps = async (
-    context
+  // export const getServerSideProps: GetServerSideProps = async (
+  context
 ) => {
-    const id = context.query.id as string | undefined;
+  const id = context.query.id as string | undefined;
 
-    // if (!id) return { notFound: true};
-    if (!id) return { notFound: true, props: { userInfo: undefined } };
+  // if (!id) return { notFound: true};
+  if (!id) return { notFound: true, props: { userInfo: undefined } };
 
-    const userInfo = await graphqlClient?.request(getUserByIdQuery, { id });
+  const userInfo = await graphqlClient?.request(getUserByIdQuery, { id });
 
-    if (!userInfo?.getUserById) return { notFound: true };
+  if (!userInfo?.getUserById) return { notFound: true };
 
-    return {
-        props: {
-            userInfo: userInfo.getUserById as User,
-        },
-    };
+  return {
+    props: {
+      userInfo: userInfo.getUserById as User,
+    },
+  };
 };
 
 //     return(
@@ -139,7 +168,31 @@ export default UserProfilePage;
 
 
 
-
+{/* <div className="flex justify-between items-center">
+<div className="flex gap-4 mt-2 text-sm text-gray-400">
+  <span>{props.userInfo?.followers?.length} followers</span>
+  <span>{props.userInfo?.following?.length} following</span>
+</div>
+{currentUser?.id !== props.userInfo?.id && (
+    <>
+      {/* {props?.userInfo?.followers?.findIndex(el => el?.id === currentUser?.id) < 0 ? ( */}
+//         <button
+//           onClick={handleUnfollowUser}
+//           className="bg-white text-black px-3 py-1 rounded-full text-sm"
+//         >
+//           Unfollow
+//         </button>
+//     ) : ( 
+//         <button
+//           // onClick={handleFollowUser}
+//           className="bg-white text-black px-3 py-1 rounded-full text-sm"
+//         >
+//           Follow
+//         </button>
+//     )} 
+//     </>
+//   )}
+// </div> */}
 
 
 
